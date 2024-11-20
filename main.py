6d0373501d113ddf4abcce7535f5f40184a7b881
HannaHalka <hanna.dm.galka@gmail.com>
2024-11-20 23:45:43 +0200

    added args total

--- main.py
+++ main.py
@@ -1,19 +1,16 @@
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-output', help='output file path', required=False)
+parser.add_argument('-total',type=str, help="year for total count", required=False)
 parser.add_argument('input_file', help='input file path')
-parser.add_argument('input_country', help='country')
-parser.add_argument('input_year', help='year')
 
 args = parser.parse_args()
 
 input_file = args.input_file
-input_country = args.input_country
-output_file = args.output
+input_country = args.total
 
 with open(input_file) as file:
     next_line = file.readline()
     while next_line:
-        ...
+        s = splitted = next_line.split('\t')
         next_line = file.readline()
