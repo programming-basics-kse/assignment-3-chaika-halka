ed201c3819481251f34d3610056dcb0e666f3f02
HannaHalka <hanna.dm.galka@gmail.com>
2024-11-21 14:40:21 +0200

    total final +normalization

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-total', type=str, help="year for total count", required=False)
+parser.add_argument('-total', type=str, help='year for total count', required=False)
 parser.add_argument('input_file', help='input file path')
 
 args = parser.parse_args()
@@ -14,14 +14,13 @@ with open(input_file) as file:
     next_line = file.readline()
     while next_line:
         s = splitted = next_line.split('\t')
-        team, year, medal = s[6], s[9], s[14]
-        if year == input_year and medal != "NA":
+        team, year, medal = s[6], s[9], s[14].replace('\n', '')
+        if year == input_year and medal != 'NA':
             if team not in country_medals:
-                country_medals[team] = {"Gold": 0, "Silver": 0, "Bronze": 0}
+                country_medals[team] = {'Gold': 0, 'Silver': 0, 'Bronze': 0}
             country_medals[team][medal] += 1
         next_line = file.readline()
 
-        if country_medals[team]["Gold"] == 1 |\
-           country_medals[team]["Silver"] == 1 |\
-           country_medals[team]["Bronze"] == 1:
+    for team in country_medals:
+        if any(country_medals[team][medal] == 1 for medal in ['Gold', 'Silver', 'Bronze']):
             print(f'{team} - {country_medals[team]["Gold"]} - {country_medals[team]["Silver"]} - {country_medals[team]["Bronze"]}')
