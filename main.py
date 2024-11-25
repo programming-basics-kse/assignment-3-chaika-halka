c034a3ca9239395165d8bf7f0aa10a26f4c958f0
achaika01 <achaika@kse.org.ua>
2024-11-25 16:47:50 +0200

    added dictionaries with years and amount of medal

--- main.py
+++ main.py
@@ -16,30 +16,17 @@ with open('data.tsv', 'r', encoding='utf-8') as file:
     YEARS = 9   #header.index('Year')
     MEDAL = 14  #header.index('Medal')
     TEAM = 6    #header.index('Team')
-
-    next_line = file.readline()
-
-    # years = []
-    # years_dict = {}
-    while next_line:
-        row = next_line.strip().split('\t')
-        if overall_input:
-            for o in overall_input:
-                years = []
-                years_dict = {}
-                if o == row[TEAM]:
-                    years.append(row[YEARS])
-                for year in years:
-                    if year in years_dict:
-                        years_dict[year] += 1
-                    else:
-                        years_dict[year] = 1
-            #print(f"{o}{years_dict}")
-            next_line = file.readline()
-
-
-print(years_dict)
-
-
-
-
+    if overall_input:
+        for o in overall_input:
+            years_dict = {}
+            with open(input_file, 'r', encoding='utf-8') as file:
+                header = file.readline()
+                for line in file:
+                    row = line.strip().split('\t')
+                    if o == row[TEAM]:
+                        year = int(row[YEARS])
+                        if year in years_dict:
+                            years_dict[year] += 1
+                        else:
+                            years_dict[year] = 1
+            print(f"{o}: {years_dict}")
