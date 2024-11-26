dfd89fb5568292d038fe248c3c5ae05e8de3b5c2
achaika01 <achaika@kse.org.ua>
2024-11-26 17:15:26 +0200

    overall update

--- overall.py
+++ overall.py
@@ -3,39 +3,35 @@ class Overall:
         self.input_file = input_file
         self.input_countries = overall_input
         self.years_dict = {}
+        self.results = {}
+        self.year = ''
 
     def process_data(self, overall_input):
-        with open('data.tsv', 'r', encoding='utf-8') as file:
-            header = file.readline().strip()
+        with open(self.input_file, 'r', encoding='utf-8') as file:
 
-            columns = header.split('\t')
-            NOC = 7  # header.index('NOC')
-            YEARS = 9  # header.index('Year')
-            MEDAL = 14  # header.index('Medal')
-            TEAM = 6  # header.index('Team')
-            results = {}
             if overall_input:
-                for o in overall_input:
+                for country in overall_input:
                     years_dict = {}
-                    file.seek(0)
-                    header = file.readline()
-                    for line in file:
-                        row = line.strip().split('\t')
-                        if o == row[TEAM]:
-                            if row[MEDAL] != 'NA':
-                                year = int(row[YEARS])
+
+                    next_line = file.readline()
+                    while next_line:
+                        s = splitted = next_line.split('\t')
+                        year_f, medal, team = s[9], s[14], s[6]
+                        if country == team and medal != 'NA':
+                            self.year = year_f
                             if year in years_dict:
-                                years_dict[year] += 1
+                                years_dict[self.year] += 1
                             else:
-                                years_dict[year] = 1
+                                years_dict[self.year] = 1
+                        next_line = file.readline()
+
                     max_year_medals = max(self.years_dict.values())
+
                     for year, medals in self.years_dict.items():
                         if medals == max_year_medals:
                             max_year = year
-                    results[o] = (max_year, max_year_medals)
-        return results
+                    self.results[country] = (max_year, max_year_medals)
 
     def print_max_year(self):
-        results = self.process_data()
-        for o, (max_year, max_year_medals) in results.items():
+        for o, (max_year, max_year_medals) in self.results.items():
             print(f"{o} - Year: {max_year}, Medals: {max_year_medals}")
